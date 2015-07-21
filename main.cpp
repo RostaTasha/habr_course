f25e2da19bfe75dc99374a66a5651a87d7bca885
natascha <natascha.abraschitowa@gmail.com>
2015-07-22 01:16:56 +0300

    files

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
 void color_triangle(int x0, int y0, int x1, int y1, int x2, int y2, TGAImage &image, TGAColor color);
 void color_triangle_s(int x01, int y0, int y1, int x2, int y2, TGAImage &image, TGAColor color);
+float sqrt3(int x, int y, int z);
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
@@ -12,10 +13,10 @@ const TGAColor blue   = TGAColor(0, 0,   255,   255);
 
 #define width 1000
 #define height 1000
-
+#define zeight 1000
 
 int main(int argc, char** argv) {
-	int x_0,x_1,y_0,y_1;
+	int x_0,x_1,y_0,y_1,z_0,z_1, x_2,y_2,z_2;
 
 	TGAImage image(width, height, TGAImage::RGB);
 	//image.set(50, 50, red);
@@ -28,33 +29,45 @@ int main(int argc, char** argv) {
 		image.set((int)(width*(coords[i]+1.0)/2),(int)(height*(coords[i+1]+1.0)/2), white);
 	}*/
 
-	float x_2,y_2;
-
 	for (unsigned int j=0; j<triangles.size();j+=3){
 	//for (unsigned int j=0; j<12;j+=3){
 			x_0=(int)(width*(coords[triangles[j]*3-3]+1.0)/2);
 			y_0=(int)(height*(coords[triangles[j]*3+1-3]+1.0)/2);
+			z_0= (int)(zeight*(coords[triangles[j]*3+2-3]+1.0)/2);
 			x_1=(int)(width*(coords[triangles[j+1]*3-3]+1.0)/2);
 			y_1=(int)(height*(coords[triangles[j+1]*3+1-3]+1.0)/2);
-		line(x_0, y_0, x_1, y_1, image, white);
-		x_0=(int)(width*(coords[triangles[j+1]*3-3]+1.0)/2);
-		y_0=(int)(height*(coords[triangles[j+1]*3+1-3]+1.0)/2);
-		x_1=(int)(width*(coords[triangles[j+2]*3-3]+1.0)/2);
-		y_1=(int)(height*(coords[triangles[j+2]*3+1-3]+1.0)/2);
-		x_2=x_0;
-		y_2=y_0;
-		line(x_0, y_0, x_1, y_1, image, white);
-		x_0=(int)(width*(coords[triangles[j]*3-3]+1.0)/2);
-		y_0=(int)(height*(coords[triangles[j]*3+1-3]+1.0)/2);
-		x_1=(int)(width*(coords[triangles[j+2]*3-3]+1.0)/2);
-		y_1=(int)(height*(coords[triangles[j+2]*3+1-3]+1.0)/2);
-		line(x_0, y_0, x_1, y_1, image, white);
-		color_triangle(x_0, y_0, x_1,y_1, x_2, y_2, image, TGAColor(rand()%255, rand()%255, rand()%255, 255));
+			z_1=(int)(zeight*(coords[triangles[j+1]*3+2-3]+1.0)/2);
+			x_2=(int)(width*(coords[triangles[j+2]*3-3]+1.0)/2);
+			y_2=(int)(height*(coords[triangles[j+2]*3+1-3]+1.0)/2);
+			z_2=(int)(zeight*(coords[triangles[j+2]*3+1-3]+1.0)/2);
+//		line(x_0, y_0, x_1, y_1, image, white);
+
+//		line(x_1, y_1, x_2, y_2, image, white);
+
+//		line(x_0, y_0, x_2, y_2, image, white);
+		 int norm_x=(y_1-y_0)*(z_2-z_0)-(y_2-y_0)*(z_1-z_0);
+	     int norm_y=(x_2-x_0)*(z_1-z_0)-(x_1-x_0)*(z_2-z_0);
+	     int norm_z=(x_1-x_0)*(y_2-y_0)-(x_2-x_0)*(y_1-y_0);
+
+	     int light_x=-1;
+	     int light_y=-1;
+	     int light_z=-1;
+
+	     float cos_alf = (light_x*norm_x+light_y*norm_y+light_z*norm_z)/sqrt3(norm_x,norm_y,norm_z)/sqrt3(light_x,light_y,light_z);
+
+	     int color_code = (int)((cos_alf+1.0)/2*255);
+		color_triangle(x_0, y_0, x_1,y_1, x_2, y_2, image, TGAColor(color_code,color_code, color_code, 255));
+
 	}
 
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
 	return 0;
+}
+
+
+float sqrt3(int x, int y, int z){
+return sqrtf((float)x*x+(float)y*y+(float)z*z);
 }
 
 
@@ -95,9 +108,9 @@ void color_triangle(int x0, int y0, int x1, int y1, int x2, int y2, TGAImage &im
 
 
 
-line(x0, y0, x1, y1, image, red);
-line(x0, y0, x2, y2, image, red);
-line(x2, y2, x1, y1, image, red);
+//line(x0, y0, x1, y1, image, red);
+//line(x0, y0, x2, y2, image, red);
+//line(x2, y2, x1, y1, image, red);
 
 
 int u_border=max(max(y0,y1),y2);
@@ -134,9 +147,9 @@ std::swap(x1, x2);
 std::swap(y1, y2);
 }
 
-int cross_coord=(int)((x0-x1)*(float)(y2-y1)/(x2-x1)+y1);
+int cross_coord=(int)((x0-x1)*(float)(y2-y1)/(float)(x2-x1)+y1);
 color_triangle_s(x0, y0,cross_coord, x2, y2, image, color);
-
+color_triangle_s(x0, y0, cross_coord, x1, y1, image, color);
 /*if (u_border==y0){
 std::swap(x0, x2);
 std::swap(y0, y2);
@@ -167,41 +180,35 @@ void color_triangle_s(int x01, int y0, int y1, int x2, int y2, TGAImage &image, 
 
 	if (y0>y1)  std::swap(y0, y1);
 
-	bool steep = false;
+float A1=(float)(y2-y1)/(x2-x01);
+float B1 = y1-A1*x01;
 
-	    /*if (x01>x2) {
-	        std::swap(x01, x2);
-	        std::swap(y0, y1);
-	    }*/
-	    int dx = x2-x01;
-	    int dy = y2-y0;
-	    int dy_	= y2-y1;
+float A0=(float)(y2-y0)/(x2-x01);
+float B0 = y0-A0*x01;
 
-	    int derror2 = std::abs(dy)*2;
-	    int derror2_ = std::abs(dy_)*2;
-	    int error2 = 0;
-	    int error2_ = 0;
-	    int y = y2;
-	    int y_=y2;
-	    for (int x=x2; x>=x01; x--) {
-	    		for (int t=y; t<=y_; t++)
-	            image.set(x, t, color);
-	        error2 += derror2;
-	        error2_+=derror2_;
 
-	        if (error2 > dx) {
-	            y -= (y2>y0?1:-1);
-	            error2 -= dx*2;
-	        }
 
-	        if (error2_ > dx) {
-	            y_ -= (y1>y0?1:-1);
-	            error2 -= dx*2;
-	        }
-	    }
+if (x01<x2){
+for (int t=x01; t<x2; t++){
+	for (int k = (int)(A0*t+B0); k<=(int)(A1*t+B1); k++)
+		 image.set(t, k, color);
+
+}
+
+} else{
+
+	for (int t=x2; t<x01; t++){
+		for (int k = (int)(A0*t+B0); k<=(int)(A1*t+B1); k++)
+			 image.set(t, k, color);
+
+	}
+}
 
 
 }
+
+
+
 
 
 
